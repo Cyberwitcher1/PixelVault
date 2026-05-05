c1213cca45eeea18f7589ef1f3b3d61fe48afd01
João Amaral <joaoamaral1706@gmail.com>
2026-05-05 19:16:50 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,50 @@
+while True:
+    print("||====||_Menu_||====||")
+    print("1 - Adicionar um filme")
+    print("2 - Atualizar um filme")
+    print("3 - Remover um filme")
+    print("4 - Mostrar todos os filmes")
+    print("5 - Mostrar todos os filmes por ordem alfabética")
+    print("6 - Pesquisa linear")
+    print("7 - Pesquisa binária (em dados ordenados)")
+    print("8 - Sair")
+    escolha = input("Escolha uma das opções: ")
+    
+    if escolha == "1":
+        print("cenas")
+        #ENTRETINEMENTO 
+        def criarRegistos():
+            registo_nome = input("O título: ")
+            registo_lancamento = input("O ano de lançamento: ")
+            registo_tempo = input("A longevidade: ")
+            registo_diretor = input("O diretor do filme: ")
+            registo_escritor = input("O escritor do filme: ")
+            print(registo_nome)
+        criarRegistos()
+    print("teste")
+
+    if escolha == "2":
+        print("teste")
+    if escolha == "3":
+        print("teste3")
+    if escolha == "4":
+        print("teste4")
+    if escolha == "5":
+        print("teste5")
+    if escolha == "6":
+        print("teste6")
+    if escolha == "7":
+        print("teste7")
+    if escolha == "8":
+        print("teste8")
+        break
+
+
+    
+
+
+
+        
+
+
+
